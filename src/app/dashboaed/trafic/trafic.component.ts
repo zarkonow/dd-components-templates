774cd42d65304ca37cdf3cec361204f7aae77b13
zarkonow <stojanov1987@gmail.com>
2025-04-14 19:48:44 +0200

    Add header, server status, tickets, and trafic components with initial templates and styles

---
+++ src/app/dashboaed/trafic/trafic.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trafic',
+  imports: [],
+  templateUrl: './trafic.component.html',
+  styleUrl: './trafic.component.css'
+})
+export class TraficComponent {
+
+}

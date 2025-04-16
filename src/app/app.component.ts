b9414c8149bd9c75a7a398a24a03dbdea4be3de4
zarkonow <stojanov1987@gmail.com>
2025-04-16 15:07:33 +0200

    Fix import paths for dashboard components and update dashboard-item template structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
-import { ServerStatusComponent } from "./dashboaed/server-status/server-status.component";
-import { TrafficComponent } from './dashboaed/traffic/traffic.component';
-import { TicketsComponent } from "./dashboaed/tickets/tickets.component";
+import { ServerStatusComponent } from "./dashboard/server-status/server-status.component";
+import { TrafficComponent } from './dashboard/traffic/traffic.component';
+import { TicketsComponent } from "./dashboard/tickets/tickets.component";
 
 @Component({
   selector: 'app-root',

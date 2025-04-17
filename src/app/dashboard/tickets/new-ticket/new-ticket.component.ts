21394e31410208bee1a4a2975f141656b1403ac8
zarkonow <stojanov1987@gmail.com>
2025-04-18 01:40:57 +0200

    Refactor dashboard components: update HTML structure for dashboard-item, server-status, tickets, and traffic components; add new ticket component with form functionality.

---
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-ticket',
+  imports: [],
+  templateUrl: './new-ticket.component.html',
+  styleUrl: './new-ticket.component.css'
+})
+export class NewTicketComponent {
+
+}
--- src/app/dashboard/tickets/tickets.component.ts
+++ src/app/dashboard/tickets/tickets.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { NewTicketComponent } from "./new-ticket/new-ticket.component";
 
 @Component({
   selector: 'app-tickets',
-  imports: [],
+  imports: [NewTicketComponent],
   templateUrl: './tickets.component.html',
   styleUrl: './tickets.component.css'
 })

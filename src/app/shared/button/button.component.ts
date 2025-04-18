9e0fc406e30d7f7d047e8d4ae0cb6fe6bbd2c0f2
zarkonow <stojanov1987@gmail.com>
2025-04-18 19:27:17 +0200

    Refactor button component: change selector to 'button[appButton]' and update HTML structure for consistency in header integration.

--- src/app/shared/button/button.component.ts
+++ src/app/shared/button/button.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-button',
+  selector: 'button[appButton]',
   imports: [],
   templateUrl: './button.component.html',
   styleUrl: './button.component.css'

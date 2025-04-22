639f18e3cd1df5b857d0b737dfc0d1306b9e90c4
zarkonow <stojanov1987@gmail.com>
2025-04-22 19:01:33 +0200

    Refactor component styles: replace IDs with :host selectors for better encapsulation and consistency

--- src/app/dashboard/dashboard-item/dashboard-item.component.ts
+++ src/app/dashboard/dashboard-item/dashboard-item.component.ts
@@ -4,7 +4,10 @@ import { Component, input, Input } from '@angular/core';
   selector: 'app-dashboard-item',
   imports: [],
   templateUrl: './dashboard-item.component.html',
-  styleUrl: './dashboard-item.component.css'
+  styleUrl: './dashboard-item.component.css',
+  host: {
+    class: 'dashboard-item',
+  }
 })
 export class DashboardItemComponent {
 // @Input({required: true}) image!: { src: string; alt:string; } 

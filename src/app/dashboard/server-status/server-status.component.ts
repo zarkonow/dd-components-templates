a8b8dcad340fd95a2c7648c079b60896845b6413
zarkonow <stojanov1987@gmail.com>
2025-04-26 14:32:59 +0200

    Refactor server status component: add OnDestroy lifecycle hook for cleanup and improve ngOnInit logic

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -6,25 +6,39 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css'
 })
-export class ServerStatusComponent implements OnInit {
+export class ServerStatusComponent implements OnInit, OnDestroy {
   currentStatus: 'online' | 'offline' | 'unknown' = 'offline';
-
+  private interval?: ReturnType<typeof setInterval>;
   constructor() {
-  
+
   }
 
   ngOnInit() {
-    setInterval(() => {
+    console.log('ON INIT');
+    this.interval = setInterval(() => {
       const random = Math.random()
 
       if (random < 0.5) {
         this.currentStatus = 'online'
-      }else if (random <0.9){
+      } else if (random < 0.9) {
         this.currentStatus = 'offline'
-      }else {
+      } else {
         this.currentStatus = 'unknown'
       }
 
     }, 5000)
+  }
+
+  ngAfterViewInit(): void {
+    console.log('AFFTER VIEW INIT');
+
+    //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+    //Add 'implements AfterViewInit' to the class.
+
+  }
+
+  ngOnDestroy(): void {
+    console.log('ON DESTROY');
+    clearTimeout(this.interval)
   }
 }
--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 
+import { FormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-new-ticket',
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, FormsModule],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent {
-
+onSubmit(titleElement: HTMLInputElement) {
+  console.log(titleElement);
+  
+}
 }
